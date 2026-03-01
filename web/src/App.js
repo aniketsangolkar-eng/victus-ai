a831deba4827255851f9310306614ed37ca07230
aniketsangolkar-eng <aniketsangolkar@gmail.com>
2026-03-01 10:00:32 +0530

    Added App.js for Victus Web App

---
+++ web/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import VoiceAssistant from './components/VoiceAssistant';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Victus AI Voice Assistant</h1>
+      <VoiceAssistant />
+    </div>
+  );
+}
+
+export default App;
